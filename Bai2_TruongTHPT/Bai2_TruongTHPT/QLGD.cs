53bb96cc7e6032b74f9bc7df0b4bec2b5829c6f1
truongmta190298 <camamidol@gmail.com>
2019-11-12 22:11:34 +0700

    ductruong qlgd

--- Bai2_TruongTHPT/Bai2_TruongTHPT/QLGD.cs
+++ Bai2_TruongTHPT/Bai2_TruongTHPT/QLGD.cs
@@ -13,17 +13,33 @@ namespace Bai2_TruongTHPT
 {
     public partial class QLGD : Form
     {
-        Lop l = new Lop();
-        MonHon mh = new MonHon();
-        GiaoVien gv = new GiaoVien();
-        CTGD ct = new CTGD();
-        int selection = 0;
         public QLGD()
         {
             InitializeComponent();
         }
 
-        private void btnTim_Click(object sender, EventArgs e)
+        Lop l = new Lop();
+        MonHon mh = new MonHon();
+        GiaoVien gv = new GiaoVien();
+        CTGD ct = new CTGD();
+        int selection = 0;
+        public void SetNull()
+        {
+            txtTiet.Text = "";
+            cboTenMon.Enabled = cboTenGV.Enabled = true;
+        }
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+
+            SetNull();
+            selection = 1;
+        }
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            cboTenMon.Enabled = cboTenGV.Enabled = true;
+            selection = 2;
+        }
+        private void CboTenLop_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
