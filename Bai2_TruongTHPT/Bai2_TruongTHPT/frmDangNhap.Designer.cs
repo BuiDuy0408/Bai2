dc5bf9bb81153afefdf6277b9b9cbb969b888bc4
hnahtit <ngtrungthanhcntt2@gmail.com>
2019-11-12 20:41:53 +0700

    thanh-add button name

--- Bai2_TruongTHPT/Bai2_TruongTHPT/frmDangNhap.Designer.cs
+++ Bai2_TruongTHPT/Bai2_TruongTHPT/frmDangNhap.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.label13 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label13
@@ -41,11 +43,30 @@
             this.label13.TabIndex = 12;
             this.label13.Text = "UserName :";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(81, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 21);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Pass :";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(158, 86);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(150, 20);
+            this.txtUserName.TabIndex = 18;
+            // 
             // frmDangNhap
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(545, 261);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label13);
             this.Name = "frmDangNhap";
             this.Text = "frmDangNhap";
@@ -57,5 +78,7 @@
         #endregion
 
         private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUserName;
     }
 }
