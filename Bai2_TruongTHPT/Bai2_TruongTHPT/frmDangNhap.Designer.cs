ee9bd75f0443c81e3732f62683f167cd4c436b2e
hnahtit <ngtrungthanhcntt2@gmail.com>
2019-11-12 20:40:57 +0700

    thanh-add form dang nhap

---
+++ Bai2_TruongTHPT/Bai2_TruongTHPT/frmDangNhap.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Bai2_TruongTHPT
+{
+    partial class frmDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(38, 84);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(97, 21);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "UserName :";
+            // 
+            // frmDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 261);
+            this.Controls.Add(this.label13);
+            this.Name = "frmDangNhap";
+            this.Text = "frmDangNhap";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label13;
+    }
+}
