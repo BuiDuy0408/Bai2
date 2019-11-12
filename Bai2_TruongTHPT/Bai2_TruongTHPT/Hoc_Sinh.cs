b9f7c6e67ae73f58bce130f23bd5d964e54d092b
truongmta190298 <camamidol@gmail.com>
2019-11-12 22:37:57 +0700

    ductruong frmhocsinh_sua

--- Bai2_TruongTHPT/Bai2_TruongTHPT/Hoc_Sinh.cs
+++ Bai2_TruongTHPT/Bai2_TruongTHPT/Hoc_Sinh.cs
@@ -16,5 +16,23 @@ namespace Bai2_TruongTHPT
         {
             InitializeComponent();
         }
+
+        private void TxtMa_HS_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void KhoiTao()
+        {
+            txtHoTen_HS.Enabled = txtPhuHuynh.Enabled = cbGT_HS.Enabled = cbLop.Enabled = dtpNgaySinh_HS.Enabled = txtDiaChi.Enabled = false;
+            btnThem_HS.Enabled = btnSua_HS.Enabled = btnXoa_HS.Enabled = true;
+            btnLuu_HS.Enabled = false;
+        }
+        public void Mo()
+        {
+            txtHoTen_HS.Enabled = txtPhuHuynh.Enabled = cbGT_HS.Enabled = cbLop.Enabled = dtpNgaySinh_HS.Enabled = txtDiaChi.Enabled = true;
+            btnThem_HS.Enabled = btnSua_HS.Enabled = btnXoa_HS.Enabled = false;
+            btnLuu_HS.Enabled = true;
+        }
+
     }
 }
