afef3172349ab59fd70df6945e4586934e7158e6
LuThaiHoc <50101866+LuThaiHoc@users.noreply.github.com>
2019-11-12 14:32:15 +0700

    Update Hoc_Sinh.Designer.cs
    Them Gioi tinh, lop

--- Bai2_TruongTHPT/Bai2_TruongTHPT/Hoc_Sinh.Designer.cs
+++ Bai2_TruongTHPT/Bai2_TruongTHPT/Hoc_Sinh.Designer.cs
@@ -32,6 +32,10 @@
             this.label15 = new System.Windows.Forms.Label();
             this.txtMa_HS = new System.Windows.Forms.TextBox();
             this.label16 = new System.Windows.Forms.Label();
+            this.cbLop = new System.Windows.Forms.ComboBox();
+            this.cbGT_HS = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtHoTen_HS
@@ -71,11 +75,56 @@
             this.label16.TabIndex = 51;
             this.label16.Text = "Mã HS";
             // 
+            // cbLop
+            // 
+            this.cbLop.FormattingEnabled = true;
+            this.cbLop.Location = new System.Drawing.Point(155, 208);
+            this.cbLop.Margin = new System.Windows.Forms.Padding(4);
+            this.cbLop.Name = "cbLop";
+            this.cbLop.Size = new System.Drawing.Size(235, 24);
+            this.cbLop.TabIndex = 58;
+            // 
+            // cbGT_HS
+            // 
+            this.cbGT_HS.FormattingEnabled = true;
+            this.cbGT_HS.Items.AddRange(new object[] {
+            "Nam",
+            "Nu"});
+            this.cbGT_HS.Location = new System.Drawing.Point(155, 147);
+            this.cbGT_HS.Margin = new System.Windows.Forms.Padding(4);
+            this.cbGT_HS.Name = "cbGT_HS";
+            this.cbGT_HS.Size = new System.Drawing.Size(235, 24);
+            this.cbGT_HS.TabIndex = 57;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(41, 218);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(32, 17);
+            this.label10.TabIndex = 56;
+            this.label10.Text = "Lớp";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(24, 156);
+            this.label14.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(65, 17);
+            this.label14.TabIndex = 55;
+            this.label14.Text = "Giới Tính";
+            // 
             // Hoc_Sinh
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(950, 510);
+            this.Controls.Add(this.cbLop);
+            this.Controls.Add(this.cbGT_HS);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label14);
             this.Controls.Add(this.txtHoTen_HS);
             this.Controls.Add(this.label15);
             this.Controls.Add(this.txtMa_HS);
@@ -93,5 +142,9 @@
         private System.Windows.Forms.Label label15;
         private System.Windows.Forms.TextBox txtMa_HS;
         private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.ComboBox cbLop;
+        private System.Windows.Forms.ComboBox cbGT_HS;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label14;
     }
 }
